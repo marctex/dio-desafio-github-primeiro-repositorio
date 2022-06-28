5621a9aef312b56983d9dea9dddfa4d0bbd8e906
marctex <marcelo.t.martins@hotmail.com>
2022-06-27 23:34:16 -0300

    javascript

--- modulo3/Introdução ao JavaScript/assets/js/scripts.js
+++ modulo3/Introdução ao JavaScript/assets/js/scripts.js
@@ -1,1 +1,19 @@
-var currentNumber = document.getElementById("currentNumber");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+var limite = 10;
+
+function increment(){
+  if(currentNumber < limite){    
+    currentNumberWrapper.innerHTML = ++currentNumber;
+  }   
+}
+
+function decrement(){  
+  if(currentNumber > -limite){   
+    currentNumberWrapper.innerHTML = --currentNumber;
+  }  
+}
+
+function limpar(){  
+  currentNumberWrapper.innerHTML = 0;
+}
